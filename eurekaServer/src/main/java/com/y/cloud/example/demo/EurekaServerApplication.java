2afbaf989396d55ba14fde4b1ffab520cb9f873f
yyx <yyx1025988443@gmail.com>
2017-12-19 21:37:22 +0800

    add eureka server

---
+++ eurekaServer/src/main/java/com/y/cloud/example/demo/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.y.cloud.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by y
+ * on 2017/12/19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
